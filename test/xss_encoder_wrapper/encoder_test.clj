52c84135cc086f91f5955f671f4ff7bf12553727
Kevin W. Wall <kevin.wall@rate.com>
2021-09-10 22:52:15 -0400

    Removed comments related to code review; fixed copy/paste typos in tests for nil and empty string.

--- test/xss_encoder_wrapper/encoder_test.clj
+++ test/xss_encoder_wrapper/encoder_test.clj
@@ -2,25 +2,11 @@
   (:require [clojure.test :refer :all]
             [xss-encoder-wrapper.encoder :refer :all]))
 
-;; CODE REVIEW - @paul - Given the "code as data" construction in functional programming in general and in Lisp derived
-;;    languages in particular, I think it might make more sense to strip out all the tests for nil and "" and just
-;;    collect all that into one (or 2) separate new tests (e.g., 'nil-or-empty-case') and then to test all the
-;;    'encode/for-xyz' functions there, but I didn't want to spend any more time restructuring tests since what's here
-;;    covers those cases, and I have too many other things on my plate to do than to be spending additional time on
-;;    this. But if you wish to restructure these tests that way, feel free to mention it. If I was a Clojure expert
-;;    though, that's probably how I would have designed it.
-;;
-;;    Also, feel free to suggest any additional tests.
-;;
-
-
 ;;
 ;;  These are pretty much minimal tests for sanity testing, which hopefully is all that is needed
 ;;  as the OWASP Java Encoder library has its own set of extensive unit tests as part of its code
-;;  repo in GitHub.
+;;  repo in GitHub. (I did find several Javadoc mistakes in their Encode class though.)
 ;;
-
-
 
 (deftest for-html-test
   (testing "for-html ..."
@@ -39,16 +25,16 @@
 ;;    https://www.javadoc.io/static/org.owasp.encoder/encoder/1.2.3/org/owasp/encoder/Encode.html#forCssString-java.lang.String-
 ;; * The following characters are encoded using hexadecimal encodings: U+0000 - U+001f, ", ', \, <, &, /, >, U+007f,
 ;;   line separator (U+2028), paragraph separator (U+2029).
-; * Any character requiring encoding is encoded as \xxx where xxx is the shortest hexadecimal representation of its
-;   Unicode code point (after decoding surrogate pairs if necessary). This encoding is never zero padded. Thus, for
-;   example, the tab character is encoded as \9, not \0009.
-
+;; * Any character requiring encoding is encoded as \xxx where xxx is the shortest hexadecimal representation of its
+;;   Unicode code point (after decoding surrogate pairs if necessary). This encoding is never zero padded. Thus, for
+;;   example, the tab character is encoded as \9, not \0009.
+;;
 (deftest for-css-string-test
   (testing "for-css-string ..."
     (testing "calls that return empty string"
       (testing "for-html with nil and empty string..."
-        (is (= (for-html "") ""))
-        (is (= (for-html nil) ""))))
+        (is (= (for-css-string "") ""))
+        (is (= (for-css-string nil) ""))))
     (testing "Other tests..."
       (is (= (for-css-string "<script/>\u0009\\ FOO&BAR\t\r\n'\"\u0012!@$%()=+{}[]")
              "\\3cscript\\2f\\3e \\9\\5c  FOO\\26 BAR\\9 \\d \\a\\27\\22\\12!@$%()=+{}[]")))))
@@ -57,8 +43,8 @@
   (testing "for-css-url ..."
     (testing "calls that return empty string"
       (testing "for-css-url with nil and empty string..."
-        (is (= (for-html "") ""))
-        (is (= (for-html nil) ""))))
+        (is (= (for-css-url "") ""))
+        (is (= (for-css-url nil) ""))))
     (testing "Other tests..."
       (is (= (for-css-url "background: url(http://example.com/dark-mode.jpg);")
              "background:\\20url\\28http:\\2f\\2f example.com\\2f dark-mode.jpg\\29;")))))
@@ -67,8 +53,8 @@
   (testing "for-html-attribute ..."
     (testing "calls that return empty string"
       (testing "for-html-attribute with nil and empty string..."
-        (is (= (for-html "") ""))
-        (is (= (for-html nil) ""))))
+        (is (= (for-html-attribute "") ""))
+        (is (= (for-html-attribute nil) ""))))
     (testing "Other tests..."
       (is (= (for-html-attribute "double-quote: \", single-quote: ' & smil\u00e9 \ud83d\ude00 <script-kiddies>")
              "double-quote: &#34;, single-quote: &#39; &amp; smilé 😀 &lt;script-kiddies>")))))
@@ -77,8 +63,8 @@
   (testing "for-html-unquoted-attribute ..."
     (testing "calls that return empty string"
       (testing "for-html-unquoted-attribute with nil and empty string..."
-        (is (= (for-html "") ""))
-        (is (= (for-html nil) ""))))
+        (is (= (for-html-unquoted-attribute "") ""))
+        (is (= (for-html-unquoted-attribute nil) ""))))
     (testing "Other tests..."
       (is (= (for-html-unquoted-attribute " ") "&#32;"))
       (is (= (for-html-unquoted-attribute "double-quote: \", single-quote: ' & smil\u00e9 \ud83d\ude00 <script-kiddies>")
@@ -88,8 +74,8 @@
   (testing "for-javascript ..."
     (testing "calls that return empty string"
       (testing "for-javascript with nil and empty string..."
-        (is (= (for-html "") ""))
-        (is (= (for-html nil) ""))))
+        (is (= (for-javascript "") ""))
+        (is (= (for-javascript nil) ""))))
     (testing "Other tests..."
       (is (= (for-javascript "\b\t\n\f\r\"&'/\\abc123") "\\b\\t\\n\\f\\r\\x22\\x26\\x27\\/\\\\abc123")))))
 
@@ -97,20 +83,20 @@
   (testing "for-uri-component ..."
     (testing "calls that return empty string"
       (testing "for-uri-component with nil and empty string..."
-        (is (= (for-html "") ""))
-        (is (= (for-html nil) ""))))
+        (is (= (for-uri-component "") ""))
+        (is (= (for-uri-component nil) ""))))
     (testing "Other tests..."
       (is (= (for-uri-component " ") "%20"))
-      (is (= (for-uri-component "@") "%40"))                ;; @ *is* encoded despite what the Encode.forUriComponent(String) Javadoc says.
+      (is (= (for-uri-component "@") "%40"))    ;; '@' *is* encoded despite what the Encode.forUriComponent(String) Javadoc says.
       (is (= (for-uri-component "!#$&'()*+,/:;=?@[]")
              "%21%23%24%26%27%28%29%2A%2B%2C%2F%3A%3B%3D%3F%40%5B%5D")))))
 
-;; Using same tast as 'for-html' since Javadoc for Encode.forXml(String) refers to Encode.forHtml(String)
+;; Using same test as 'for-html' since Javadoc for Encode.forXml(String) refers to Encode.forHtml(String)
 (deftest for-xml-test
   (testing "for-xml ..."
     (testing "calls that return empty string"
       (testing "for-xml with nil and empty string..."
-        (is (= (for-html "") ""))
-        (is (= (for-html nil) ""))))
+        (is (= (for-xml "") ""))
+        (is (= (for-xml nil) ""))))
     (testing "Other tests..."
-      (is (= (for-html "&<>\"'") "&amp;&lt;&gt;&#34;&#39;")))))
+      (is (= (for-xml "&<>\"'") "&amp;&lt;&gt;&#34;&#39;")))))
